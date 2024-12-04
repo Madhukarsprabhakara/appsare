9c0b768f287a99e0bf27fed8629223bf30c7be81
Madhukar Prabhakara <msprabhakara@gmail.com>
2024-12-04 01:24:17 -0800

    url error

--- src/resources/js/Pages/Trackers/Partials/CreateTrackerForm.vue
+++ src/resources/js/Pages/Trackers/Partials/CreateTrackerForm.vue
@@ -28,7 +28,7 @@
                 <input type="text" name="url" id="url" v-model="form.url" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="" />
 
               </div>
-              <InputError v-if="$page.props.errors" :message="$page.props.errors.url" class="mt-2" />
+              
               <p class="mt-1 text-sm leading-6 text-gray-600">You may track non-secured sites using http:// instead of https://</p>
             </div>
           </div>
