dbaf29133dab62b23af817e84c5f9a2653aff75d
Madhukar Prabhakara <msprabhakara@Madhukars-MacBook-Pro.local>
2024-12-06 23:52:04 -0800

    500 response status handling fixed

--- src/app/Jobs/WebsiteStatusCheck30.php
+++ src/app/Jobs/WebsiteStatusCheck30.php
@@ -28,21 +28,40 @@ class WebsiteStatusCheck30 implements ShouldQueue
     public function handle(): void
     {
        
-        //fire head request
-        $start_time = microtime(true);
-        $response = Http::retry(3, 100)->head($this->url);
-        $end_time = microtime(true);
-        $response_time = $end_time - $start_time;
-        $response_time*1000;
-        //insert into tracker_event table
-        $tracker_event=new TrackerEvent;
-        $tracker_event->tracker_id=$this->tracker_id;
-        $tracker_event->response_time=$response_time;
-        $tracker_event->epoch_start_time=$start_time;
-        $tracker_event->epoch_end_time=$end_time;
-        $tracker_event->http_status_code=$response->status();
-        $tracker_event->message='Success';
-        $tracker_event->save();
+       try {
+            //fire head request
+            $start_time = microtime(true);
+            $response = Http::retry(3, 100)->head($this->url);
+            $end_time = microtime(true);
+            $response_time = $end_time - $start_time;
+            $response_time*1000;
+            //insert into tracker_event table
+            $tracker_event=new TrackerEvent;
+            $tracker_event->tracker_id=$this->tracker_id;
+            $tracker_event->response_time=$response_time;
+            $tracker_event->epoch_start_time=$start_time;
+            $tracker_event->epoch_end_time=$end_time;
+            if ($response->status()>=200 && $response->status()<=399)
+            {
+               $tracker_event->message='Success'; 
+            }
+            else
+            {
+                $tracker_event->message='Failed'; 
+            }
+            $tracker_event->http_status_code=$response->status();
+            $tracker_event->save();
+       }
+       catch (\Exception $e)
+       {
+            $response = $e->response;
+            $tracker_event=new TrackerEvent;
+            $tracker_event->tracker_id=$this->tracker_id;
+            $tracker_event->http_status_code=$response->status();
+            $tracker_event->message=$e->getMessage();
+            $tracker_event->save();
+       }
+        
 
     }
 }
