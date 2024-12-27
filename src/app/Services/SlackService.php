9daa3f0fe715f68e4ffa15b1d7e7b834b9e603de
Madhukar Prabhakara <msprabhakara@Madhukars-MacBook-Pro.local>
2024-12-27 00:52:59 -0800

    slack implementation - removed chown from php.prod.dockerfile

--- src/app/Services/SlackService.php
+++ src/app/Services/SlackService.php
@@ -23,7 +23,7 @@ class SlackService {
     public function saveSlackToken($team_id)
     {
         $slack_user = Socialite::driver('slack')->asBotUser()->user();
-        return $slack_user->token;
+        return $slack_user;
         $team=Team::where('id', $team_id)->first();
         if ($team)
         {
