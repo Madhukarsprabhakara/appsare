24f092917138fefc0f41cfd7307c62ba027b0649
Madhukar Prabhakara <msprabhakara@Madhukars-MacBook-Pro.local>
2025-01-14 07:20:33 -0800

    Layout padding and margin fixes

--- src/resources/js/Layouts/AppLayoutv2.vue
+++ src/resources/js/Layouts/AppLayoutv2.vue
@@ -291,7 +291,7 @@
                 </div>
             </div>
 
-            <main class="py-10 bg-gray-100 max-h-fit px-10">
+            <main class="bg-gray-100 max-h-fit pb-5 px-10">
                 <!-- Page Heading -->
                 <header v-if="$slots.header" class=" ">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
