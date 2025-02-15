175885cd38c5be56d4f5fdfbb101e475e75adc89
Madhukar Prabhakara <msprabhakara@Madhukars-MacBook-Pro.local>
2025-02-15 11:42:12 -0800

    Go coroutines with head requests working

--- go/main.go
+++ go/main.go
@@ -13,6 +13,45 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 func performTask() {
 	// Your specific task goes here
 	fmt.Println("Performing the task at", time.Now())
+
+	// Example list of URLs to check
+	urls := []string{
+		"http://msprabhakara.com",
+		"http://demo.sopact.com",
+		"http://ffp.sopact.com",
+		"http://tbb.sopact.com",
+		"http://kennedycenter.sopact.com",
+		"http://survey.sopact.com",
+		"http://sense-test.sopact.com",
+		"http://ffafrica.sopact.com",
+		"http://icff.sopact.com",
+		"http://blackinnovation.sopact.com",
+		"http://crossroads.sopact.com",
+		"http://kuramo.sopact.com",
+	}
+
+	// Create a channel to synchronize the completion of all goroutines
+	done := make(chan bool, len(urls))
+
+	// Perform HTTP GET requests concurrently
+	for _, url := range urls {
+		go func(url string) {
+			resp, err := http.Head(url)
+			if err != nil {
+				fmt.Printf("Error checking %s: %v\n", url, err)
+			} else {
+				fmt.Printf("Checked %s: %d at %s\n", url, resp.StatusCode, time.Now())
+				resp.Body.Close()
+			}
+			// Signal that this goroutine has completed
+			done <- true
+		}(url)
+	}
+
+	// Wait for all goroutines to complete
+	for i := 0; i < len(urls); i++ {
+		<-done
+	}
 }
 
 func main() {
@@ -21,6 +60,7 @@ func main() {
 		fmt.Println("Starting the server on :3000...")
 		http.ListenAndServe(":3000", nil)
 	}()
+
 	// Create a ticker that triggers every 30 seconds
 	ticker := time.NewTicker(30 * time.Second)
 	defer ticker.Stop()
